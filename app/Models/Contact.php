61aefe0238c0151ad353a36cf28befec58f1472a
zhfrann <muhzhafranilham13@gmail.com>
2025-06-06 11:01:37 +0700

    feat: add address model & migration

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Contact extends Model
 {
@@ -13,8 +14,13 @@ class Contact extends Model
     public $timestamps = true;
     public $incrementing = true;
 
-    public function users(): BelongsTo
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, "user_id", "id");
+    }
+
+    public function addresses(): HasMany
+    {
+        return $this->hasMany(Address::class, "contact_id", "id");
     }
 }
