cb51cc2d3b16b3d56f4e53a867ad35076c52643b
zhfrann <muhzhafranilham13@gmail.com>
2025-06-08 17:25:20 +0700

    feat: get contact api

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Contact;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,13 @@ class ContactSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $user = User::query()->where('username', '=', 'test')->first();
+        Contact::query()->create([
+            'user_id' => $user->id,
+            'first_name' => 'test',
+            'last_name' => 'test',
+            'email' => 'test@gmail.com',
+            'phone' => '12345'
+        ]);
     }
 }
