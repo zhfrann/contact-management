28dd4a87f23a25c2758569b0dad77eea84059c09
zhfrann <muhzhafranilham13@gmail.com>
2025-06-09 09:17:07 +0700

    feat: update address api

--- routes/api.php
+++ routes/api.php
@@ -32,8 +32,10 @@ Route::middleware(ApiAuthMiddleware::class)->group(function () {
 
     Route::post('/contacts/{idContact}/addresses', [AddressController::class, 'create'])
         ->where('idContact', '[0-9]+');
-
     Route::get('/contacts/{idContact}/addresses/{idAddress}', [AddressController::class, 'get'])
+        ->where('idContact', '[0-9]+')
+        ->where('idAddress', '[0-9]+');
+    Route::put('/contacts/{idContact}/addresses/{idAddress}', [AddressController::class, 'update'])
         ->where('idContact', '[0-9]+')
         ->where('idAddress', '[0-9]+');
 });
